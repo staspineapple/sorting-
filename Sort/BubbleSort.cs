a65ee849a718ea43e31a4c0f7c18d4bad95de6f5
stas <Stas.pineapplee@yandex.ru>
2021-05-24 12:59:47 +0300

    Added BubleSort

---
+++ Sort/BubbleSort.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class BubbleSort
+    {
+        int[] a;
+        public BubbleSort(int[] a)
+        {
+            this.a = a;
+        }
+
+        public void Sort()
+        {
+            
+            int temp;
+            int CountOfChange = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                for (int j = 0; j < a.Length-1-i; j++)
+                {
+
+
+                    if (a[j+1] < a[j])
+                    {
+                        temp = a[j + 1];
+                        a[j + 1] = a[j];
+                        a[j] = temp;
+                        CountOfChange++;
+                    }
+                }
+                Console.WriteLine($"{i} заход");
+                Print();
+                Console.WriteLine($"{i} конец");
+
+                if (CountOfChange==0)
+                {
+                    break;
+
+                }
+
+                CountOfChange = 0;
+            }
+
+
+            
+        }
+        
+        public void Print()
+        {
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Sort/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] a = { 9,8,7,6,5,4,3,2,1,0,9,8,7,6,5,4,3,2,1 };
+            BubbleSort b = new BubbleSort(a);
+            b.Sort();
+            b.Print();
+        }
+    }
+}
